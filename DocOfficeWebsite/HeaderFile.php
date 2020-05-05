7fabb4892a403b5c5e8d68360f5d3f61cba7c856
Jesse Smith <jessrosela@csu.fullerton.edu>
2020-05-05 06:10:08 -0700

    Modified the HeaderFile and the style sheet
    the header file code didn't look clean to me and there seemed to be a lot of style code that wasn't in the style sheet file so I took all the styles from header and added to the style sheet and I think this makes the header file easier to read

--- DocOfficeWebsite/HeaderFile.php
+++ DocOfficeWebsite/HeaderFile.php
@@ -1,54 +1,34 @@
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
 <title>Doctor's Office</title>
-<link href="kanika-style.css" rel="stylesheet" type="text/css" />
+<link rel="stylesheet" type="text/css" href="kanika-style.css"/>
+<meta charset="UTF-8" />
 </head>
-<style type="text/css">
-<!--
-body {
-	margin-left: 0px;
-	margin-top: 0px;
-}
-img.one {
-  height: 70%;
-  width: 100%;
-}
-
-img.two {
-  height: 50%;
-  width: 60%;
-}
-.style3 {
-	color: #FF0000;
-	font-weight: bold;
-}
--->
-</style></head>
 
 <head>
-<h1><center> Welcome to CPSC 332 </center> </h1></head>
-<hr><b><center>A Doctor's Office Website</center></b><hr>
-<center> <p><b> By Matthew Farah and Jesse Smith</center>
-</b>
-<body>
-<table width="1350" border="0" align="center" cellpadding="15px" cellspacing="0">
+<h1><center> Welcome to CPSC 332 </center> </h1>
+<hr><b><center>A Doctor's Office Website</center></hr>
+<hr>
+<center> by Matthew Farah and Jesse Smith</center></b></hr>
+</p></b>
+</head>
 
-  <tr>
-    <td colspan="2"><img class = "one" src="england.jpg" width="100" height="230" /></td>
-  </tr>
-  <tr>
-    <td colspan="2" bgcolor="#DBE2E8"><strong>&nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp; <a href="ViewDocSpecialty.php">Doctors and their Specialties</a>&nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp;  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;  &nbsp; &nbsp; &nbsp;  &nbsp;   </strong><strong> <a href="ViewDocStevens.php">Patients of Doctor Robert Stevens</a>&nbsp; &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp; &nbsp; &nbsp;   &nbsp; &nbsp; &nbsp;  &nbsp;
-		 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp;    <a href="ViewDocVicodin.php">Doctors Who've Prescribed Vicodin</a>  &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;   &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;  &nbsp;<a href="starter.php">Home</a>   </strong></td>
-  </tr>
+<body>
+
+
+<table id="t01">
+	<tr><img class = "one" src="england.jpg" alt="View of London" style="width:100%"/>
+  		<th><a href="ViewDocSpecialty.php">Doctors and Their Specialties</a>
+  		</th>
+   		 <th><a href="ViewDocStevens.php">Patients of Doctor Robert Stevens</a>
+    	</th>
+    	<th><a href="ViewDocVicodin.php">Doctors Who've Prescribed Vicodin</a>
+		</th>
+		<th><a href="starter.php">Home</a>
+		</th>
+	</tr>
 </table>
 
 </body>
 </html>
-
-<html>
-<head>
-<body bgcolor="CCCCCC">
-<hr>
